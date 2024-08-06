07b7890a86073a9b31df2fbd0087587e1f55beca
akobets <ank4kobets@yandex.ru>
2024-08-06 08:35:38 +0300

    adding solve_quiz_and_get_code() method

--- pages/base_page.py
+++ pages/base_page.py
@@ -24,3 +24,25 @@ class BasePage:
         else:
             return True
 
+
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        time.sleep(1)
+        alert.accept()
+        WebDriverWait(self.browser, 15).until(EC.alert_is_present())
+        try:
+            alert = self.browser.switch_to.alert
+            WebDriverWait(self.browser, 15).until(EC.alert_is_present())
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
+
+
--- test_product_page.py
+++ test_product_page.py
@@ -3,10 +3,11 @@ import time
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.add_to_basket()
+    prod_page.solve_quiz_and_get_code()
+    time.sleep(5)
     prod_page.check_right_title()
     prod_page.check_right_price()
-    time.sleep(2)
