1bead8faa9092cf6fb21d2098516e3646575df2f
akobets <ank4kobets@yandex.ru>
2024-08-07 19:47:02 +0300

    editing test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from .pages.main_page import MainPage
@@ -7,37 +9,37 @@ from .pages.basket_page import BasketPage
 
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage():
+class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         main_page = MainPage(browser, MainPageLocators.MAIN_LINK)
         main_page.open()
         main_page.go_to_login_page()
-        login_url = browser.current_url
-        self.guest_can_see_login_and_regist_forms(browser, login_url)
+        time.sleep(3)
 
-    def guest_can_see_login_and_regist_forms(self, browser, login_url):
+        login_url = browser.current_url
         login_page = LoginPage(browser, login_url)
         login_page.open()
         login_page.should_be_login_url(login_url)
         login_page.should_be_login_form()
         login_page.should_be_register_form()
+        time.sleep(3)
 
     def test_guest_should_see_login_link(self, browser):
         main_page = MainPage(browser, MainPageLocators.MAIN_LINK)
         main_page.open()
         main_page.should_be_login_link()
+        time.sleep(3)
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     main_page = MainPage(browser, MainPageLocators.MAIN_LINK)
     main_page.open()
     main_page.go_to_basket()
+    time.sleep(3)
+
     basket_url = browser.current_url
-    guest_see_empty_basket(browser, basket_url)
-
-
-def guest_see_empty_basket(browser, basket_url):
     basket_page = BasketPage(browser, basket_url)
     basket_page.open()
     basket_page.basket_is_empty()
     basket_page.should_be_empty_basket_message()
+    time.sleep(3)
